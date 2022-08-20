341c333e699edf3465171b14d1e13728faed4ae8
Amir-Abdullah <69315783+Amir-Abdullah@users.noreply.github.com>
2022-08-20 20:20:05 +0600

    update-0.0.1

---
+++ JS/c_api.js
@@ -0,0 +1,112 @@
+const errorText = document.getElementById('error-message');
+errorText.style.display = 'none';
+const phoneDetails = document.getElementById('phone-details');
+
+
+const phoneSearch = () => {
+    const searchFieldValue = document.getElementById('search-field');
+    const phoneSearchText = searchFieldValue.value;
+    searchFieldValue.value = '';
+    // error handling
+    if (phoneSearchText == '') {
+        errorText.style.display = 'block';
+        errorText.innerHTML = "Error search box is empty, Please search something"
+    } 
+    // serach result handle
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(responseData => displaySearchResult(responseData.data))
+    }
+}
+
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    phoneDetails.innerHTML = "";
+    if (phones.length == 0) {
+        errorText.innerHTML = "According to your search no phone was found"
+        errorText.style.display = 'block';
+    }
+    (phones.slice(0, 20)).forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4');
+        // phone search Result as html
+        div.innerHTML = `
+            <div class="border bg-light p-2 rounded"  >
+            <img class=" mx-auto d-block m-2" src="${phone.image}"/>
+                <h5> ${phone.phone_name}</h5>
+                <p> Brand : ${phone.brand}</p>
+                <div class="d-grid">
+                    <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-info">Details</button>
+                </div>
+            </div>
+        `
+        searchResult.appendChild(div);
+        errorText.style.display = 'none';
+    })
+}
+
+const loadPhoneDetail = (slug) => {
+
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+const displayPhoneDetail = (details) => {
+    console.log(details.mainFeatures);
+    const div = document.createElement('div');
+    div.classList.add('card');
+    let mainFeatures = details.mainFeatures ? details.mainFeatures : '';
+    let sensor = details.mainFeatures.sensors ? details.mainFeatures.sensors : '';
+    let other = details.others ? details.others : "";
+    phoneDetails.innerHTML = "";
+    window.scrollTo(0, 50);
+    // single phone details as html
+    div.innerHTML = `
+    <div class="card-body bg-light rounded ">
+    <img src="${details.image}" class="card-img-top mx-auto d-block w-50 my-2" alt="...">
+        <h5 class="card-title"> Device Name : ${details.name}</h5>
+        <p class="card-text"> <strong>Brand</strong> : ${details.brand}</p>
+        <p class="card-text"> <strong>Release Date</strong> : ${details.releaseDate ? details.releaseDate : "No release Date found" }</p>
+        <h5><strong>Main Features</strong></h5>
+        <ul class="list-group list-unstyled">
+            <li>ChipSet : ${mainFeatures.chipSet ? mainFeatures.chipSet : " "}</li>
+            <li>Display-Size : ${mainFeatures.displaySize ? mainFeatures.displaySize : " "}</li>
+            <li>Memory : ${mainFeatures.memory ? mainFeatures.memory : " "}</li>
+            <li>Storage : ${mainFeatures.storage ? mainFeatures.storage : " "}</li>
+        </ul>
+        <br>
+        <h5><strong>Sensors</strong></h5>
+        <ul class="list-group list-unstyled">
+            <li> ${sensor[0] ? sensor[0] : " "}</li>
+            <li> ${sensor[1] ? sensor[1] : " "}</li>
+            <li> ${sensor[2] ? sensor[2] : " "}</li>
+            <li> ${sensor[3] ? sensor[3] : " "}</li>
+            <li> ${sensor[4] ? sensor[4] : " "}</li>
+            <li> ${sensor[5] ? sensor[5] : " "}</li>
+        </ul>
+        <br>
+        <h5><strong>Others</strong></h5>
+        <ul class="list-group list-unstyled">
+            <li>Bluetooth : ${other.Bluetooth ? other.Bluetooth : "No Data Found"}</li>
+            <li>GPS : ${other.GPS ? other.GPS : "No Data Found"}</li>
+            <li>NFC : ${other.NFC ? other.NFC : "No Data Found"} </li>
+            <li>Radio : ${other.Radio ? other.Radio : "No Data Found" }</li>
+            <li>USB : ${other.USB ? other.USB : "No Data Found"} </li>
+            <li>WLAN : ${other.WLAN ? other.WLAN : "No Data Found"} </li>
+            <li class="d-flex flex-row-reverse mt-2 align-items-center">
+                <button type="button" class="order-1 mx-1 w-25 btn btn-lg btn-primary">Buy Now</button>
+                <h6 class="order-3 mx-1 d-flex section-title ">Quantity</h6>
+                <div class="order-2 mx-1">
+                    <input type="text" step="1" min="1" max="5" autocomplete="off" height="100%" width="5" placeholder="In Stock: ">
+                </div>
+            </li>
+        </ul>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # School database management project
+<div class="section-content">
+    <div class="next-number-picker next-number-picker-inline">
+        <div class="next-number-picker-handler-wrap">
+            <a unselectable="unselectable" class="next-number-picker-handler next-number-picker-handler-up ">
+                <span unselectable="unselectable" class="next-number-picker-handler-up-inner">
+                    <i class="next-icon next-icon-add next-icon-medium"></i>
+                </span>
+            </a>
+            <a unselectable="unselectable" class="next-number-picker-handler next-number-picker-handler-down next-number-picker-handler-down-disabled">
+                <span unselectable="unselectable" class="next-number-picker-handler-down-inner">
+                    <i class="next-icon next-icon-minus next-icon-medium"></i>
+                </span>
+            </a>
+        </div>
+        <div class="next-number-picker-input-wrap">
+            <span class="next-input next-input-single next-input-medium next-number-picker-input">
+                <input type="text" step="1" min="1" max="5" value="1" autocomplete="off" height="100%">
+            </span>
+        </div>
+    </div>
+    <span class="quantity-content-default"></span>
+</div>
